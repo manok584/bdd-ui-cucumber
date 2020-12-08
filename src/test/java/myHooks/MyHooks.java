46e96e010d9345410dcac60d07973b809ecba310
manok584 <manohar.kamarushi@gmail.com>
2020-12-08 16:30:31 +0530

    cucumber maven reporting

--- src/test/java/myHooks/MyHooks.java
+++ src/test/java/myHooks/MyHooks.java
@@ -4,9 +4,9 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
+import io.cucumber.core.api.Scenario;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
 
 public class MyHooks {
 	WebDriver driver;
--- src/test/java/testRunners/DashboardTest.java
+++ src/test/java/testRunners/DashboardTest.java
@@ -9,11 +9,11 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
 		features= {"src/test/resources/appfeatures"},
 		glue= {"stepDefinitions", "src/test/java/myHooks"},
-		plugin= {"pretty", "html:target/cucumber-reports/report.html"},
+		plugin= {"pretty", "json:target/cucumber-reports"},
 		monochrome= true,
 		dryRun = true,
-		tags = "@Regression",
-		publish = false  // only available in 6.9.0
+		tags = "@Regression"
+//		publish = false  // only available in 6.9.0
 		)
 public class DashboardTest {
 
